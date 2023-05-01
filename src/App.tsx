6b9bb1d919413298936d7e793e6d3378daa3e71a
tigawanna <denniskinuthiaw@gmail.com>
2023-05-01 11:47:31 +0300

    add card example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { TypographyDemo } from './components/TypographyDemo'
+import { CardDemo } from './components/CardDemo'
+
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen">
-    <TypographyDemo/>
+    <div className="min-h-screen flex justify-center items-center">
+      <CardDemo/>
     </div>
   )
 }
