7edc1fe74533620c7e2d821d1e5a9800a60678c2
tigawanna <denniskinuthiaw@gmail.com>
2023-05-01 10:54:03 +0300

    typograghy example

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { TypographyDemo } from './components/TypographyDemo'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="h-screen bg-accent text0g text-accent-foreground">
-     hello
+    <div className="min-h-screen">
+    <TypographyDemo/>
     </div>
   )
 }
